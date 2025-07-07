35d797c6c352199b892dca99b6a26d5c7adf71be
Bbrian223 <barreragomezb2800@gmail.com>
2025-07-07 11:10:17 -0300

    feat: Add WeatherController

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WeatherApp.Services;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+
+        private readonly WeatherServices _climaService = new WeatherServices();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(string ciudad)
+        {
+            var result = await _climaService.GetWeatherAsync(ciudad);
+            return View(result);
+        }
+    }
+}
--- WeatherApp/Services/WeatherServices.cs
+++ WeatherApp/Services/WeatherServices.cs
@@ -6,31 +6,32 @@ using System.Web;
 using System.Net.Http;
 using Newtonsoft.Json;
 using WeatherApp.Models;
+using System.Configuration;
 
 namespace WeatherApp.Services
 {
     public class WeatherServices
     {
         private readonly HttpClient _httpClient;
-        private readonly string _apiKey = "API_KEY";
+        private readonly string _apiKey = Environment.GetEnvironmentVariable("OPENWEATHER_API_KEY");
 
         public WeatherServices()
         {
             _httpClient = new HttpClient();
         }
 
-        public async Task<WeatherResponse> ObtenerClimaPorCiudadAsync(string ciudad)
+        public async Task<WeatherResponse> GetWeatherAsync(string city)
         {
-            string url = $"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={_apiKey}&units=metric";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={_apiKey}&units=metric";
 
             var response = await _httpClient.GetAsync(url);
 
             if (!response.IsSuccessStatusCode) return null;
 
             string json = await response.Content.ReadAsStringAsync();
-            WeatherResponse clima = JsonConvert.DeserializeObject<WeatherResponse>(json);
+            WeatherResponse weather = JsonConvert.DeserializeObject<WeatherResponse>(json);
 
-            return clima;
+            return weather;
         }
 
 
