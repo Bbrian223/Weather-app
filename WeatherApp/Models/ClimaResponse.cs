afab72fcd5ef0b410f7b3e52a33912897fb035fc
Bbrian223 <barreragomezb2800@gmail.com>
2025-07-07 02:35:30 -0300

    Add model ClimaResponse for weather api

---
+++ WeatherApp/Models/ClimaResponse.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherApp.Models
+{
+    public class ClimaResponse
+    {
+        public Coord coord { get; set; }
+        public List<Weather> weather { get; set; }
+        public MainData main { get; set; }
+        public Wind wind { get; set; }
+        public string name { get; set; }
+
+    }
+
+
+    public class Coord
+    {
+        public float lon { get; set; }
+        public float lat { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class MainData
+    {
+        public float temp { get; set; }
+        public float feels_like { get; set; }
+        public float temp_min { get; set; }
+        public float temp_max { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+    }
+
+    public class Wind
+    {
+        public float speed { get; set; }
+        public int deg { get; set; }
+    }
+
+
+
+}
